1c1ef1e424efe729b6778844d707c218eee9ebcf
ferllini13 <ferllini_13@hotmail.com>
2018-10-10 18:40:07 -0600

    querrys requeridos listos

--- src/api.py
+++ src/api.py
@@ -10,46 +10,94 @@ Movies = DB["Movies"]
 Companies=DB["Companies"]
 
 
+
+#elimnar
+#def mDel():
+#def cDel():
+
+
+#agregar
+def mAdd(title,genre,director_name,franchise,country,year,duration,company,actors):
+    data={
+	    "title":title,
+		"genre":genre,
+		"director_name":director_name,
+		"franchise":franchise,
+		"country":country,
+		"year":int(year), 
+		"duration":int(duration), 
+		"company":company, 
+		"actors":actors
+    }
+
+    Movies.insert_one(data)
+
+
+def cAdd(name,year,web_address):
+    data={
+		"name":name,
+		"year":year,
+		"web_address":web_address  
+    }
+
+    Companies.insert_one(data)
+
+
+
+
+#editar 
+#def mEdit():
+#def cEdit():
+
+
+
 #Consultar toda la información de la película con un título en particular.
 def movieByTittle(title):
     movie = Movies.find_one({"title":title})
-    print(movie)
     return movie
 
 #Consultar  toda  la  información  de  las  películas  de  una  franquicia  en particular.
 def movieByFranchise(franchise):
-    movie = Movies.find({"franchise":franchise})
-    print(movie)
+    movie = Movies.find_one({"franchise":franchise})
     return movie
 
 
 #Consultar toda la información de las películas estrenadas en un rango de años específico.
-#def movieByRange(yearRange):
+def movieByRange(year1,year2):
+    movie = Movies.find_one({ "$and": [{"year":{"$gte":year1}}, {"year":{"$lte":year2}}]})
+    return movie
+
 
 #Consultar el nombre de película, género, año de estreno de todas las películas producidas por una compañía productora en particular.
-#def movieByCompany(Company):
+def movieByCompany(Company):
+    movie = Movies.aggregate ([{"$match" : { "company" : Company }},{"$project" : {"title":1, "genre":1, "year":1 }}]) 
+    return list(movie)
 
-#Consultar la cantidad de películas
-#def cantMovies():
 
-#Consultar la película con la menor duración 
-def lessDuration():
-    movie = Movies.find().sort("duration", 1).limit(0)
-    print(movie[0])
+#Consultar la cantidad de películas de compañia e  las películas producidas por una compañía productora en particular
+def cantMovies(Company):
+    cant = Movies.find({"company":Company}).count()
+    return cant
+
+#Consultar la película con la menor duración e  las películas producidas por una compañía productora en particular
+def lessDuration(Company):
+    movie = Movies.find({"company":Company}).sort("duration", 1).limit(0)
     return movie[0]
 
-#Consultar la  película  con  la  mayor  duración
-def moreDuration():
-    movie = Movies.find().sort("duration", -1).limit(0)
-    #print(movie[0])
+#Consultar la  película  con  la  mayor  duracióne  las películas producidas por una compañía productora en particular
+def moreDuration(Company):
+    movie = Movies.find({"company":Company}).sort("duration", -1).limit(0)
     return movie[0]
 
 #Consultar la  duración  promedio  de  las películas producidas por una compañía productora en particular
-#def averageDuration(Company)
+def averageDuration(Company):
+    cant = Movies.aggregate([{"$group":{"_id": "$company","average": { "$avg": "$duration" }}}]) 
+    return list(cant)
 
 
 def main():
-    movie = lessDuration()
-    print(movie)
+    shit = averageDuration("UK Films")
+    print (shit)
+
 
 main()
--- src/populate.py
+++ src/populate.py
@@ -9,59 +9,59 @@ Companies=DB["Companies"]
 
 movies = [
     {
-							"title":"Ant-Man",
-							"genre":"Action",
-							"director_name":"Peyton Reed",
-							"franchise":"",
-							"country":"USA",
-							"year":int(2015), 
-							"duration":117, 
-							"company":"Marvel Studios", 
-							"actors":[{"name":"Paul Rudd"},{"name":"Michael Douglas"},{"name":"Corey Stoll"}]
+		"title":"Ant-Man",
+		"genre":"Action",
+		"director_name":"Peyton Reed",
+		"franchise":"",
+		"country":"USA",
+		"year":int(2015), 
+		"duration":117, 
+		"company":"Marvel Studios", 
+		"actors":[{"name":"Paul Rudd"},{"name":"Michael Douglas"},{"name":"Corey Stoll"}]
     },
     {
-							"title":"A Clockwork Orange",
-							"genre":"Crime",
-							"director_name":"Stanley Kubrick",
-							"franchise":"",
-							"country":"England",
-							"year":int(1971), 
-							"duration":136, 
-							"company":"UK Films", 
-							"actors":[{"name":"Malcolm McDowell"},{"name":"Patrick Magee"},{"name":"Michael Bates"}]
+		"title":"A Clockwork Orange",
+		"genre":"Crime",
+		"director_name":"Stanley Kubrick",
+		"franchise":"",
+		"country":"England",
+		"year":int(1971), 
+		"duration":136, 
+		"company":"UK Films", 
+		"actors":[{"name":"Malcolm McDowell"},{"name":"Patrick Magee"},{"name":"Michael Bates"}]
     },
     {
-							"title":"Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-							"genre":"Comedy",
-							"director_name":"Stanley Kubrick",
-							"franchise":"",
-							"country":"England",
-							"year":int(1964), 
-							"duration":95, 
-							"company":"UK Films", 
-							"actors":[{"name":"Peter Sellers"},{"name":"George C. Scott"},{"name":"Sterling Hayden"}]
+		"title":"Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+		"genre":"Comedy",
+		"director_name":"Stanley Kubrick",
+		"franchise":"",
+		"country":"England",
+		"year":int(1964), 
+		"duration":95, 
+		"company":"UK Films", 
+		"actors":[{"name":"Peter Sellers"},{"name":"George C. Scott"},{"name":"Sterling Hayden"}]
     },
     {
-							"title":"X-Men: First Class",
-							"genre":"Action & Sci-Fi",
-							"director_name":"Matthew Vaughn",
-							"franchise":"X-Men Franchise",
-							"country":"USA",
-							"year":int(2011), 
-							"duration":131, 
-							"company":"Marvel Studios", 
-							"actors":[{"name":"James McAvoy"},{"name":"Michael Fassbender"},{"name":"Jennifer Lawrence"}]
+		"title":"X-Men: First Class",
+		"genre":"Action & Sci-Fi",
+		"director_name":"Matthew Vaughn",
+		"franchise":"X-Men Franchise",
+		"country":"USA",
+		"year":int(2011), 
+		"duration":131, 
+		"company":"Marvel Studios", 
+		"actors":[{"name":"James McAvoy"},{"name":"Michael Fassbender"},{"name":"Jennifer Lawrence"}]
     },
     {
-							"title":"X-Men: Days of Future Past",
-							"genre":"Action & Sci-Fi",
-							"director_name":"Bryan Singer",
-							"franchise":"X-Men Franchise",
-							"country":"USA",
-							"year":int(2014), 
-							"duration":132, 
-							"company":"Marvel Studios", 
-							"actors":[{"name":"Patrick Stewart"},{"name":"Hugh Jackman"},{"name":"Ian McKellen"}]
+		"title":"X-Men: Days of Future Past",
+		"genre":"Action & Sci-Fi",
+		"director_name":"Bryan Singer",
+		"franchise":"X-Men Franchise",
+		"country":"USA",
+		"year":int(2014), 
+		"duration":132, 
+		"company":"Marvel Studios", 
+		"actors":[{"name":"Patrick Stewart"},{"name":"Hugh Jackman"},{"name":"Ian McKellen"}]
     }
 ]
 
